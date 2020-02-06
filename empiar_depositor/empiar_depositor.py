158dd7ecce7d468b1fb92ff9decbedac57262508
andreyyudin <andrii@ebi.ac.uk>
2020-02-06 16:25:29 +0000

    Setup.py adjusted to faciliate the additional files

--- empiar_depositor/empiar_depositor.py
+++ empiar_depositor/empiar_depositor.py
@@ -19,6 +19,7 @@ specific language governing permissions and limitations
 under the License.
 
 Version history
+1.6b14, 20200206, Andrii Iudin: Setup.py adjusted to faciliate the additional files.
 1.6b13, 20200206, Andrii Iudin: Schema and tests are now a part of the Python module.
 1.6b12, 20200206, Andrii Iudin: Added optional output of the entry ID and the directory name.
 1.6b11, 20200114, Andrii Iudin: Basic Authentication is allowed as an alternative to Token Authentication.
@@ -419,7 +420,7 @@ sition_1.json ~/Downloads/micrographs
     empiar-depositor -r 10 ABC123 -e ~/Downloads/dep_thumb.png 0123456789 -g 01234567-89a-bcde-fghi-jklmnopqrstu ~/Docu\
 ments/empiar_deposition_1.json ~/Downloads/micrographs
                 """
-        version = "1.6b13"
+        version = "1.6b14"
 
         parser = argparse.ArgumentParser(prog=prog, usage=usage, add_help=False,
                                          formatter_class=argparse.RawTextHelpFormatter)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='empiar-depositor',
-    version='1.6b13',
+    version='1.6b14',
     packages=find_packages(),
     author="Andrii Iudin",
     author_email="andrii@ebi.ac.uk, andrii.iudin@gmail.com",
@@ -19,6 +19,7 @@ setup(
     long_description=long_description,
     license="Apache License",
     keywords="EMPIAR, deposition, microscopy",
+    include_package_data=True,
     install_requires=["requests"],
     classifiers=[
         # maturity
